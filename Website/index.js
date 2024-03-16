5399df594a83af849b1078d12e1ecd7f0d5e63f0
Shayan Parsai <70860248+ShayanParsai@users.noreply.github.com>
2024-03-16 18:01:56 +0100

    Update - 20% of learning phase

--- Website/index.js
+++ Website/index.js
@@ -1,5 +1,4 @@
 
-let timesresetted = 0;
 let username;
 let age;
 let daystravelingcount = 7;
@@ -7,13 +6,17 @@ let isValidAge = false;
 const MinimumAge = 18;
 
 document.getElementById("loginbutton").onclick = function() {
-    username=document.getElementById("usernametext").value;
-    console.log(username);
-    document.getElementById("usernamelabel").innerHTML ="Welcome "+username;
+
+    username = document.getElementById("usernametext").value;
+    username = username.replaceAll("r","l");
+    document.getElementById("usernamelabel").innerHTML = "Salam " + username.slice(0,username.indexOf(" "));
+
     while (!isValidAge) { // If user is under 18, they cant enter the website
         let input = window.prompt("PLEASE VERIFY YOUR AGE");  
         if (input === null) { 
             console.log("User cancelled the prompt.");
+            document.body.innerHTML = "<h1>You need to be 18+ to enter<br><br>This website includes spicy content. "+ 
+            "<br> If you wish to travel to thailand, please consult with your parents. <br><br><br> Or just get some age kid</h1>";
             return; 
         }
         age = Number(input);
@@ -23,30 +26,24 @@ document.getElementById("loginbutton").onclick = function() {
             isValidAge = true;
         }
     }
-    console.log(age);
     if (age < MinimumAge) { // Validates users age
         document.body.innerHTML = "<h1>You need to be 18+ to enter<br><br>This website includes spicy content. "+ 
         "<br> If you wish to travel to thailand, please consult with your parents. <br><br><br> Or just get some age kid</h1>";
     }
+    document.getElementById("usernametext").value = ""; // Clear the input box after submittion
 }
 
-document.getElementById("increasebutton").onclick = function() {
+document.getElementById("increasebutton").onclick = function() { // Increase button
     document.getElementById("daystraveling").innerHTML = daystravelingcount +=1;
-    if (daystravelingcount == 15) {
-        window.alert("Yeye, Cash Finns min broder");
+}
+document.getElementById("decreasebutton").onclick = function() { // Decrease button
+    if (daystravelingcount == 1) {
+        window.alert("You cant travel for less than one day");
+    } else {
+        document.getElementById("daystraveling").innerHTML = daystravelingcount -=1;
     }
 }
-document.getElementById("decreasebutton").onclick = function() {
-    document.getElementById("daystraveling").innerHTML = daystravelingcount -=1;
-    if (daystravelingcount == 3) {
-        window.alert("De är para knas eller?");
-    }
-}
-document.getElementById("resetbutton").onclick = function() {
+document.getElementById("resetbutton").onclick = function() { // Reset button
     document.getElementById("daystraveling").innerHTML = daystravelingcount = 7;
-    timesresetted +=1;
-    if (timesresetted == 5) {
-        window.alert("Lak Bestämm dig, antigen res eller res inte.");
-    }
 }
 
